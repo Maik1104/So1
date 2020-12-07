61c2d5e0d16148aeb8e84120f2147d997d04468e
Michael Zapata <mizapataa@unal.edu.co>
2020-12-07 05:29:47 -0500

    a

--- a/a/views.py
+++ a/a/views.py
@@ -541,7 +541,7 @@ def modificarPropietario(request):
 
 def otraRuta(request):
 
-    busqueda = request.POST["nuevaRuta"]
+    busqueda = request.POST["rutanueva"]
     if busqueda == "":
         busqueda = "."
         ubicacion = getoutput("pwd")
